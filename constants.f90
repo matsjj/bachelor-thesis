89733c5bc89d91d82ec29d657d1afe2c225a8df5
matsjj <matsjj@gmail.com>
2014-02-04 20:27:52 +0100

    Create constants.f90

---
+++ constants.f90
@@ -0,0 +1,4 @@
+real *8                             ::              PI = 3.14159265359
+real *8                             ::              EULER = 2.71828182846
+real *8                             ::              MUE_NULL = 12.566370614E-7
+real *8                             ::              EPSILON_NULL = 8.854187817E-12
